a1f7bc9da511391477b0fc2028d2db60aa61c76a
Mudit Ameta <zeusdeux@gmail.com>
2024-02-09 21:56:19 +0100

    Rename functions to make better sense of them

--- src/my_addon.c
+++ src/my_addon.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include "./my_addon.h"
 
-static napi_value Method(napi_env env, napi_callback_info info)
+static napi_value Hello(napi_env env, napi_callback_info info)
 {
   napi_status status;
   napi_value world;
@@ -13,10 +13,10 @@ static napi_value Method(napi_env env, napi_callback_info info)
 #define DECLARE_NAPI_METHOD(name, func)         \
   { name, 0, func, 0, 0, 0, napi_default, 0 }
 
-static napi_value Init(napi_env env, napi_value exports)
+static napi_value Export(napi_env env, napi_value exports)
 {
   napi_status status;
-  napi_property_descriptor desc = DECLARE_NAPI_METHOD("hello", Method);
+  napi_property_descriptor desc = DECLARE_NAPI_METHOD("hello", Hello);
   status = napi_define_properties(env, exports, 1, &desc);
   assert(status == napi_ok);
   return exports;
@@ -24,5 +24,5 @@ static napi_value Init(napi_env env, napi_value exports)
 
 napi_value create_my_addon(napi_env env, napi_value exports)
 {
-  return Init(env, exports);
+  return Export(env, exports);
 }
